d864de9788c03a343b8499db6a3cfccc31b4a0ba
FStriewski <fstriewski@outlook.com>
2018-04-30 19:15:43 +0200

    Add bar chart

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <D3BarChart />
+        <D3BarChart data={[5, 8, 1, 3]} size={[300, 300]}/>
       </div>
     );
   }
--- src/charts/D3BarChart.jsx
+++ src/charts/D3BarChart.jsx
@@ -1,30 +1,57 @@
-import React, { Component } from 'react';
-import * as d3 from "d3";
-
-export default class D3BarChart extends Component {
-
-    render(){
-
-        let data = [4, 8, 15, 16, 23, 42];
-
-        const output = d3.select("#chartd3")
-            .selectAll("div")
-            .data(data)
-            .enter().append("div")
-            .style("width", function (d) {
-                return d * 10 + "px";
-            })
-            .text(function (d) { return d; });
+import React, { Component } from 'react'
+import { scaleLinear, max, select } from 'd3'
 
 
+class D3BarChart extends Component {
+    constructor(props) {
+        super(props)
+        this.createBarChart = this.createBarChart.bind(this)
+    }
+    componentDidMount() {
+        this.createBarChart()
+    }
+    componentDidUpdate() {
+        this.createBarChart()
+    }
+    createBarChart() {
+        const node = this.node
+        const dataMax = max(this.props.data)
+        const yScale = scaleLinear()
+            .domain([0, dataMax])
+            .range([0, this.props.size[1]])
 
+        select(node)
+            .selectAll('rect')
+            .data(this.props.data)
+            .enter()
+            .append('rect')
+
+        select(node)
+            .selectAll('rect')
+            .data(this.props.data)
+            .exit()
+            .remove()
+
+        select(node)
+            .selectAll('rect')
+            .data(this.props.data)
+            .style('fill', '#fe6622')
+            .attr('x', (d, i) => i * 25)
+            .attr('y', d => this.props.size[1]- yScale(d))
+            .attr('height', d => yScale(d))
+            .attr('width', 25)
+            .attr("style", "outline: thin solid blue;")  
+
+    }
+    render() {
         return (
-            <div id="chartd3"> Test
-            
-            
+            <div id="barChart">
+                        <h3>Simple BarChart </h3>
+                        <svg ref={node => this.node = node}
+                        width={500} height={500}>
+                    </svg>
             </div>
         )
     }
-
-
 }
+export default D3BarChart
